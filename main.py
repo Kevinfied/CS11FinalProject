5ccf681b53cb3428004e3099307833f66d772c31
Kevin <realkevinx@gmail.com>
2023-05-30 11:09:21 -0400

    eeee

--- main.py
+++ main.py
@@ -1,5 +1,39 @@
-
+import pygame
 import tank
-__name__ = "__main__"
+import os
+import os
 
+os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (-10, 100)
+
+mainRunning = True
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 768
+pygame.display.set_caption("FSE")
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+
+
+while mainRunning:
+    rightShoot,leftShoot = False, False
+    for evt in pygame.event.get():
+        if evt.type == pygame.QUIT:
+            mainRunning = False
+        if evt.type == pygame.KEYDOWN:
+            if evt.key == pygame.K_c:
+                leftShoot = True
+            if evt.key == pygame.K_SLASH:
+                rightShoot = True
+
+    keyArray = pygame.key.get_pressed()
+
+    screen.fill((155,155,155))
+    tank.tankLeft.update(keyArray[pygame.K_w],keyArray[pygame.K_s],keyArray[pygame.K_a],keyArray[pygame.K_d],leftShoot)
+    tank.tankRight.update(keyArray[pygame.K_UP],keyArray[pygame.K_DOWN],keyArray[pygame.K_LEFT],keyArray[pygame.K_RIGHT], rightShoot)
+
+
+
+    pygame.display.flip()
+    pygame.time.Clock().tick(60)
+
+pygame.quit()
+quit()
 

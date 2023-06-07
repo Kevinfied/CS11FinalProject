0ecd50433141ccdd51a91788a80101c8316e9fe0
Raymond131 <98779905+Raymond131@users.noreply.github.com>
2023-06-07 09:11:54 -0400

    boumm

--- main.py
+++ main.py
@@ -31,8 +31,8 @@ while mainRunning:
     keyArray = pygame.key.get_pressed()
 
     screen.fill((155,155,155))
-    tank.tankLeft.update(keyArray[pygame.K_w],keyArray[pygame.K_s],keyArray[pygame.K_a],keyArray[pygame.K_d],leftShoot)
-    tank.tankRight.update(keyArray[pygame.K_UP],keyArray[pygame.K_DOWN],keyArray[pygame.K_LEFT],keyArray[pygame.K_RIGHT], rightShoot)
+    # tank.tankLeft.update(keyArray[pygame.K_w],keyArray[pygame.K_s],keyArray[pygame.K_a],keyArray[pygame.K_d],leftShoot)
+    # tank.tankRight.update(keyArray[pygame.K_UP],keyArray[pygame.K_DOWN],keyArray[pygame.K_LEFT],keyArray[pygame.K_RIGHT], rightShoot)
     if tank.tankLeft.update(keyArray[pygame.K_w],keyArray[pygame.K_s],keyArray[pygame.K_a],keyArray[pygame.K_d],leftShoot) == 'end' or tank.tankRight.update(keyArray[pygame.K_UP],keyArray[pygame.K_DOWN],keyArray[pygame.K_LEFT],keyArray[pygame.K_RIGHT], rightShoot) == 'end':
         pygame.mixer.Sound.play(assets.deathExplosion)
         pygame.time.delay(3000)

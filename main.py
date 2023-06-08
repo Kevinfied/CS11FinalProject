5b03673f76212ccb4bb5f409559a444a8baa44d2
Kevin <realkevinx@gmail.com>
2023-06-08 09:58:08 -0400

    Commit by iota

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ while mainRunning:
     screen.fill((155,155,155))
     for map in level.map1:
         pygame.draw.rect(screen, (0,0,0), map)
-    
+
     # tank.tankLeft.update(keyArray[pygame.K_w],keyArray[pygame.K_s],keyArray[pygame.K_a],keyArray[pygame.K_d],leftShoot)
     # tank.tankRight.update(keyArray[pygame.K_UP],keyArray[pygame.K_DOWN],keyArray[pygame.K_LEFT],keyArray[pygame.K_RIGHT], rightShoot)
     if tank.tankLeft.update(keyArray[pygame.K_w],keyArray[pygame.K_s],keyArray[pygame.K_a],keyArray[pygame.K_d],leftShoot) == 'end' or tank.tankRight.update(keyArray[pygame.K_UP],keyArray[pygame.K_DOWN],keyArray[pygame.K_LEFT],keyArray[pygame.K_RIGHT], rightShoot) == 'end':

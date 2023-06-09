28513939924d343e1926662ce2f55f9a9c7447d9
Raymond131 <98779905+Raymond131@users.noreply.github.com>
2023-06-09 09:19:18 -0400

    Co-authored-by: Kevin <Kevinfied@users.noreply.github.com>

--- tank.py
+++ tank.py
@@ -1,11 +1,12 @@
 from pygame import *
 from math import *
 import assets
-
+import copy
 init()
 RED = (255,0,0)
 GREEN = (0,255,0)
-
+BLACK = (0,0,0)
+BLUE = (0,0,255)
 
 def shoelace(points):
     downsum, upsum = 0,0
@@ -47,13 +48,13 @@ def smartRect(x,y,r,spinRad, point):
         draw.polygon(screen,GREEN,points)
 
 
+
 joystick.init()
 # joysticks = [joystick.Joystick(x) for x in range(joystick.get_count())]
 screen = display.set_mode((1024,768))
 HEIGHT = screen.get_height()
 WIDTH = screen.get_width()
-BLACK = (0,0,0)
-BLUE = (0,0,255)
+
 
 bulletLife = 6000 #bullet lives for 6000 miliseconds/6sec
 reloadPeriod = 5000 #reload time 
@@ -69,6 +70,9 @@ redTank = image.load('assets/redTankNorm.png')
 # returns the velocity components (x and y), given a direction and magnitude (heading and  d)
 def velComponents(heading,d):
     return d*cos(heading+pi/2) , -d*sin(heading + pi/2)
+
+
+
 
 class Tank:
     def __init__(self,surf, img, x, y, angle, col, scale, name):
@@ -98,17 +102,7 @@ class Tank:
         
             
 
-        if left:
-            self.angle += self.angVel
-        elif right:
-            self.angle -= self.angVel
         
-        if forward:
-            self.x = (self.x + self.mag*cos(self.angle  + pi/2) + WIDTH) % WIDTH
-            self.y = (self.y - self.mag*sin(self.angle  + pi/2) + HEIGHT) % HEIGHT
-        elif back:
-            self.x = (self.x - self.mag*cos(self.angle  + pi/2) + WIDTH) % WIDTH
-            self.y = (self.y + self.mag*sin(self.angle  + pi/2) + HEIGHT) % HEIGHT
 
         rotated_image = transform.rotate(self.img, degrees(self.angle))
         rotatedCenter = (self.offsetDown * cos(self.angle+pi/2) + self.x,  -self.offsetDown * sin(self.angle + pi/2) + self.y)
@@ -116,24 +110,37 @@ class Tank:
 
 
         self.surf.blit(rotated_image, bounding_rect)
-        basePoints = []
+        self.basePoints = []
         fatPoints = []
+
         h1 = (21*2**0.5) * self.scale
         h2 = (21**2 + 31**2)**0.5 * self.scale
-
         crookedAngle  = 0.975386
-        theta = [pi/4, 3*pi/4, pi+crookedAngle, 2*pi-crookedAngle]
+        theta1 = 0.278300
+        theta2 = 0.191184
+        theta = [pi/4, 3*pi/4, pi+crookedAngle, 2*pi-crookedAngle] + [pi/2 - theta1, theta1 + pi/2, pi/2 - theta2, pi/2 + theta2]
+
+        
+        mh1 = (21**2+6**2)**0.5 * self.scale
+        mh2 = (31**2+6**2)**0.5 * self.scale
+
 
         for i in range(4):
             if i//2 == 0:
-                basePoints.append([rotatedCenter[0]+ h1* cos(theta[i] + self.angle), rotatedCenter[1] - h1 * sin(theta[i] + self.angle)])
+                self.basePoints.append([rotatedCenter[0]+ h1* cos(theta[i] + self.angle), rotatedCenter[1] - h1 * sin(theta[i] + self.angle)])
                 fatPoints.append([rotatedCenter[0]+ (h1+self.bulletRad)* cos(theta[i] + self.angle), rotatedCenter[1] - (h1+self.bulletRad) * sin(theta[i] + self.angle)])
             else:
-                basePoints.append([rotatedCenter[0]+ h2* cos(theta[i] + self.angle), rotatedCenter[1] - h2 * sin(theta[i] + self.angle)])
+                self.basePoints.append([rotatedCenter[0]+ h2* cos(theta[i] + self.angle), rotatedCenter[1] - h2 * sin(theta[i] + self.angle)])
                 fatPoints.append([rotatedCenter[0]+ (h2+self.bulletRad)* cos(theta[i] + self.angle), rotatedCenter[1] - (h2+self.bulletRad) * sin(theta[i] + self.angle)])
         
+        
+        for i in range(4,8):
+            if i//6 == 0:
+                self.basePoints.append([rotatedCenter[0] + mh1* cos(theta[i] + self.angle), rotatedCenter[1] - mh1 * sin(theta[i] + self.angle)])
+            else:
+                self.basePoints.append([rotatedCenter[0] + mh2* cos(theta[i] + self.angle), rotatedCenter[1] - mh2* sin(theta[i] + self.angle)])
 
-        for point in basePoints:
+        for point in self.basePoints:
             draw.circle(self.surf, self.col, point, 3)
         for point in fatPoints:
             draw.circle(self.surf, self.col, point, 3)
@@ -144,8 +151,30 @@ class Tank:
         
         
         draw.circle(self.surf, self.col, (self.x, self.y), 3)
-        # draw.rect(self.surf, self.col, new_rect, 2)
+        draw.circle(self.surf, self.col, rotatedCenter, 3) 
+        draw.rect(self.surf, self.col, bounding_rect, 2)
         
+
+        movement = [0,0,0]
+        if left:
+            self.angle += self.angVel
+            movement[0] = self.angVel
+        elif right:
+            self.angle -= self.angVel
+            movement[0] = -self.angVel
+        if forward:
+            self.x = (self.x + self.mag*cos(self.angle  + pi/2) + WIDTH) % WIDTH
+            self.y = (self.y - self.mag*sin(self.angle  + pi/2) + HEIGHT) % HEIGHT
+            movement[1] = self.mag*cos(self.angle  + pi/2)
+            movement[2] = - self.mag*sin(self.angle  + pi/2)
+        elif back:
+            self.x = (self.x - self.mag*cos(self.angle  + pi/2) + WIDTH) % WIDTH
+            self.y = (self.y + self.mag*sin(self.angle  + pi/2) + HEIGHT) % HEIGHT
+            movement[1] = - self.mag*cos(self.angle  + pi/2)
+            movement[2] = self.mag*sin(self.angle  + pi/2)
+
+
+        # print(movement)
         if shooting and self.loads != 0:
             mixer.Sound.play(assets.pop)
             muzX, muzY = (fatPoints[0][0]+fatPoints[1][0])/2 + self.bulletRad*cos(self.angle+pi/2), (fatPoints[0][1]+fatPoints[1][1])/2 - self.bulletRad * sin(self.angle +pi/2)
@@ -153,18 +182,15 @@ class Tank:
             self.shots.append([muzX,muzY,vx,vy,time.get_ticks()])
             self.loads -= 1
         if 0<= time.get_ticks()%5000 <= margin:
-            print('reloaded')
             self.loads = loads
         # print(time.get_ticks()%5000)
         for shot in self.shots:
             if shot[X]  <= 0 or shot[X]>=screen.get_width():
-
                 shot[VX] = -shot[VX]
                 mixer.Sound.play(assets.ping)
             if shot[Y] <= 0 or shot[Y] >= screen.get_height():
                 shot[VY] = -shot[VY]
                 mixer.Sound.play(assets.pong)
-
             shot[X] += shot[VX]
             shot[Y] += shot[VY]
         for i in range(len(self.shots)):  
@@ -176,7 +202,6 @@ class Tank:
             if 0 <= shot[X] <= screen.get_width() and 0 <= shot[Y] <= screen.get_height():
                 draw.circle(screen, self.col, shot[:2], self.bulletRad)
         for tank in tanks:
-            # print(tank)
             if tank != self:
                 for i in range(len(tank.shots)):  
                     shot = tank.shots[i]
@@ -185,15 +210,16 @@ class Tank:
                         return 'end'
         
 
-        
-                
 
-
-        
 # changed from 5 to 3 and the circle is now in the wrong position. 
 tankLeft = Tank(screen, assets.redBase, 200, screen.get_height()/2, 0, RED, 3, 'player1')
 tankRight = Tank(screen, assets.blackBase, 800, screen.get_height()/2, 0, BLACK, 3, 'player2')
 tanks = [tankLeft, tankRight]
+                
+
+
+        
+
 
 
 
@@ -201,13 +227,13 @@ tanks = [tankLeft, tankRight]
 # player2 = Tank(screen, assets.redBase, 800, screen.get_height()/2, 0, BLUE, 5)
 # bot = Tank(screen, assets.blackBase, 800, screen.get_height()/2, 0, BLUE, 5)
 
-def moveTank(surf, image, rad, x, y):
-    rotated_image = transform.rotate(image, degrees(rad))
-    offsetToDown = 13
-    rotatedCenter = (offsetToDown * cos(rad+pi/2) + x,  -offsetToDown * sin(rad + pi/2) + y+offsetToDown)
-    new_rect = rotated_image.get_rect(center = rotatedCenter)
-    surf.blit(rotated_image, new_rect)
-    draw.circle(surf, (0,0,0), (x, y+offsetToDown), 5)
+# def moveTank(surf, image, rad, x, y):
+#     rotated_image = transform.rotate(image, degrees(rad))
+#     offsetToDown = 13
+#     rotatedCenter = (offsetToDown * cos(rad+pi/2) + x,  -offsetToDown * sin(rad + pi/2) + y+offsetToDown)
+#     new_rect = rotated_image.get_rect(center = rotatedCenter)
+#     surf.blit(rotated_image, new_rect)
+#     draw.circle(surf, (0,0,0), (x, y+offsetToDown), 5)
 
 
 def collision(tank, obj):
